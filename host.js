f69c08cd112f7cd037bb4643a755278b0b759002
Pascal Stephens <43835186+GimmeMahHamburger@users.noreply.github.com>
2025-03-17 21:46:20 -0700

    Shrunk host, added favicon

--- host.js
+++ host.js
@@ -2,16 +2,26 @@ import { createServer } from 'http'
 import { readFile } from 'fs'
 const port = 45678
 
+const dict = {
+    "/":["index.html","text/html"],
+    "/index.html":["index.html","text/html"],
+    "/WARNING.png":["WARNING.png","image/png"],
+    "/favicon.ico":["favicon.png","image/png"]
+}
+
 const server = createServer(function(req, res){
 
     function sendStuff(fileTitle, fileType){
         console.log("asked for:");
         console.log(fileTitle);
+        console.log("of type:");
+        console.log(fileType);
         
         readFile(fileTitle, function(error,data){
             if(error){
                 res.writeHead(404)
-                res.write("File Not Found")
+                res.write("File Not Found: " + req.url)
+                console.log("could not find " + req.url)
             }else{
                 res.writeHead(200, { 'Content-Type' : fileType })
                 res.write(data)
@@ -20,16 +30,26 @@ const server = createServer(function(req, res){
             return;
         })
     }
+    
+    console.log(Object.keys(dict))
+    console.log(req.url)
+    console.log(dict[req.url])
+    sendStuff(dict[req.url][0],dict[req.url][1]);
+    
+/*
     if (req.url === "/" || req.url === "/index.html") {
         sendStuff('index.html','text/html');
         return;
     } else if (req.url === "/WARNING.png") {
         sendStuff('WARNING.png','image/png');
         return;
+    } else if (req.url === "/favicon.ico") {
+        sendStuff('favicon.png','image/png');
+        return;
     } else {
         console.log("We don't have "+req.url);
     }
-
+*/
 
     
 })
